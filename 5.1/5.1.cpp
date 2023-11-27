cb38ec6e76b101920bb1cc1c1ecf7cbdb0753802
Компьютер <@DESKTOP-0TR3ASG>
2023-11-27 19:53:10 +0700

    Добавьте файлы проекта.

---
+++ 5.1/5.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <clocale>
+/*Дан массив A размера N (N — нечетное число).
+Вывести его элементы с нечетными номерами в порядке убывания номеров:
+AN , AN −2, AN −4, . . ., A1.*/
+
+int main()
+{
+    const size_t N = 9;
+    int a[N];
+    for (int i = 0; i < N; ++i) { // заполняем массив
+        std::cin >> a[i];
+    }
+    for (int i = N - 1; i >= 0; i -= 2) // индекс начинается с нуля
+        std::cout << a[i] << " ";
+    return 0;
+}
